3c4ffc4e9b986bf9876de7ca60c1da12f914d219
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-17 17:20:58 +0000

    view hit points

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.feature "Entry form"  do
+feature "Entry form"  do
   scenario "displays players names" do
     visit("/")
 
@@ -10,5 +10,15 @@ RSpec.feature "Entry form"  do
 
     expect(page).to have_content('Mitch')
     expect(page).to have_content('Mica')
+  end
+end
+
+feature "Hit points" do
+  scenario "player 1 can see player 2's" do
+    visit('/')
+
+    fill_in :player2, with: 'Mica'
+    click_button 'submit'
+    expect(page).to have_content('Mica: 60HP')
   end
 end
