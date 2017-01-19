c4928c41476cc05835c22c41ebf0c19a5b762896
Stefan Liute <stefanliute@gmail.com>
2017-01-19 15:16:22 +0000

    Multiplayer

--- spec/features/attack1_spec.rb
+++ spec/features/attack1_spec.rb
@@ -1,14 +1,31 @@
 require 'spec_helper'
 
-feature 'player 1 attack' do
+feature '#attack' do
   scenario 'player 1 attacks player 2' do
     sign_in_and_play
     click_link('attack')
-    expect(page).to have_text('Confirmed!')
+    expect(page).to have_text('Mitch attacked Mica.')
   end
+
   scenario "player 1 reduces player2's points" do
     sign_in_and_play
     click_link('attack')
-    expect(page).to have_content(50)
+    expect(page).to have_content('Mica points: 50')
+  end
+
+  scenario 'player 2 attacks player 1' do
+    sign_in_and_play
+    click_link('attack')
+    click_link('OK')
+    click_link('attack')
+    expect(page).to have_text('Mica attacked Mitch.')
+  end
+
+  scenario "player 2 reduces player 1's points" do
+    sign_in_and_play
+    click_link('attack')
+    click_link('OK')
+    click_link('attack')
+    expect(page).to have_content('Mitch points: 50')
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -5,4 +5,9 @@ feature "Hit points" do
     sign_in_and_play
     expect(page).to have_content('Mica: 60HP')
   end
+
+  scenario "player 1 can see their own" do
+    sign_in_and_play
+    expect(page).to have_content('Mitch: 60HP')
+  end
 end
